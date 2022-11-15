dae66e632b5e513894ab520d2a31bd2235553720
Daniil <fedorov_danilka@bk.ru>
2022-11-15 21:51:28 +0500

    v-icon & auto height
    Added <v-icon> for spacer
    Fixed height of tag block

--- src/components/ui/TagItem/TagItem.vue
+++ src/components/ui/TagItem/TagItem.vue
@@ -5,15 +5,22 @@
 			:class="['tag-item__list d-flex flex-wrap', {
 				'justify-space-between': tag.alignment == 'width'
 			}]"
+			:style="{'height': `${height}px`}"
 		>
 			<div 
 				:class="['d-flex align-center tag-item__tag', {
 					'tag-item__tag--width': tag.alignment == 'width'
 				}]" 
-				v-for="tagItem in tag.tagList" 
+				v-for="(tagItem, index) in tag.tagList" 
 				:key="tagItem.text"
 			>
-				<div class="tag-item__info d-flex align-center">
+				<v-icon v-if="index !== 0" class="tag-item__spacer ma-auto">
+					mdi-circle-small
+				</v-icon>
+				<div 
+					class="tag-item__info d-flex align-center" 
+					ref="tagItem"
+				>
 					<v-icon class="tag-item__icon mr-1" v-if="tagItem.icon">
 						{{ tagItem.icon }}
 					</v-icon>
@@ -30,6 +37,24 @@ export default {
 		tag: {
 			type: Object,
 			required: true
+		}
+	},
+
+	data() {
+		return {
+			ro: null,//ResizeObserver
+			height: 0
+		}
+	},
+
+	mounted() {
+		this.ro = new ResizeObserver(this.resizeBlock)
+		this.ro.observe(this.$refs.tagItem[0])
+	},
+
+	methods: {
+		resizeBlock() {
+			this.height = this.$refs.tagItem[0].offsetHeight
 		}
 	},
 
